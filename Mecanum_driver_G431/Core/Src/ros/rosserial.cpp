9bf1afb2ded66c3db7301ee4e102202084c9af9c
wsadzxc23 <279158508@qq.com>
2024-05-31 23:48:11 +0800

    获取ros下发小乌龟的控制消息的通信验证

--- Mecanum_driver_G431/Core/Src/ros/rosserial.cpp
+++ Mecanum_driver_G431/Core/Src/ros/rosserial.cpp
@@ -4,15 +4,28 @@
  *  Created on: May 21, 2024
  *      Author: wang
  */
-
+#include "stdio.h"
+#include "string.h"
 #include "rosserial/rosserial.h"
 #include <ros.h>
 #include <std_msgs/String.h>
+#include<geometry_msgs/Twist.h>
 
 ros::NodeHandle nh;
 
 std_msgs::String str_msg;
 ros::Publisher chatter("chatter", &str_msg);
+
+void chassis_ctl_callback(const geometry_msgs::Twist& cmd_vel)
+{
+//  char buf[128] = {0};
+//  snprintf(buf, sizeof(buf),"twist msg:vx:%f, vy:%f, vw:%f\r\n", cmd_vel.linear.x, cmd_vel.linear.x, cmd_vel.angular.z);
+//  str_msg.data = buf;
+//  chatter.publish(&str_msg);
+  printf("twist msg:vx:%f, vy:%f, vw:%f\r\n", cmd_vel.linear.x, cmd_vel.linear.x, cmd_vel.angular.z);
+}
+
+ros::Subscriber<geometry_msgs::Twist> sub_cmdvel("turtle1/cmd_vel", &chassis_ctl_callback );
 char hello[] = "i have changed source location!!!\r\n";
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
@@ -27,16 +40,13 @@ void setup(void)
 {
   nh.initNode();
   nh.advertise(chatter);
+  nh.subscribe(sub_cmdvel);
 }
 
 void loop(void)
 {
 
-  str_msg.data = hello;
-  chatter.publish(&str_msg);
   nh.spinOnce();
-
-  HAL_Delay(1000);
 }
 
 
